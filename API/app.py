20e4e5c1b13f31db32d0258f378c665863d78547
Pako <u17076146@tuks.co.za>
2020-07-22 21:39:26 +0200

    API is operational and ML indexes the DB

--- API/app.py
+++ API/app.py
@@ -1,12 +1,13 @@
 # Dependencies
 from flask import Flask, request, jsonify
 import pandas as pd
-import numpy as np
+import numpy as pd
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 import re
 import nltk
 from nltk.stem.porter import PorterStemmer
+import db
 
 # load model
 model = pickle.load(open('model.pkl', 'rb'))
@@ -14,14 +15,13 @@ model = pickle.load(open('model.pkl', 'rb'))
 # API definition
 app = Flask(__name__)
 
-# routes
-@app.route("/predict", methods=['POST'])
+# Middleware
 def predict():
     # get data
     data = request.get_json(force=True)
     print("Original: ", data)
     log_entry = data['entry']
-    print("Value: ", log_entry)
+    #print("Value: ", log_entry)
 
     # convert data to 2d matrix
     corpus = []
@@ -33,19 +33,40 @@ def predict():
     #print("Corpus what what: ", log_entry)
 
     corpus.append(log_entry)
-    print("Corpus what what:", corpus)
+    #print("Corpus what what:", corpus)
     countVectorizer = CountVectorizer(max_features = 1500)
     X = countVectorizer.fit_transform(corpus).toarray()
-    np
+    
+    X = X.transpose()
 
     y_pred = model.predict(X)
-    print("Prediction please God: ", y_pred)
     
+    kb_index = y_pred[0]
+    #print("index", y_pred[0])
+    #print("Prediction please God: ", y_pred)
+
     # send back to client
-    output = {'prediction': int(y_pred[0]) }
+    #output = {'prediction': int(y_pred[0]) }
 
     # return data
-    return jsonify(results=output)
+    #return jsonify(results=output)
+    return int(kb_index)
+
+def fetch_data(index):
+    #db.db.kb_articles.insert_one({"name": "John"})
+    queryObject = {'kb_index': index}
+    res = db.db.kb_articles.find_one(queryObject) 
+    res.pop('_id')
+    res.pop('kb_index')
+    return jsonify(res)
+
+# routes
+@app.route("/predict", methods=['POST'])
+def analyze():
+    index = predict()
+    result = fetch_data(index)
+    #result['status'] = "success"
+    return result
 
 if __name__ == '__main__':
     app.run(port=6667, debug=True)
