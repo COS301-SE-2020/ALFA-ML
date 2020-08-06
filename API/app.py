7b21d83d6551527b2b483c9a723a1551861ca2cc
Pako Diale <u17076146@tuks.co.za>
2020-08-07 01:47:02 +0200

    API changed back to receiving JSON object but will different structure

--- API/app.py
+++ API/app.py
@@ -26,8 +26,8 @@ app = Flask(__name__)
 def predict():
     # get data
     # decode base64 file
-    log_file = base64.b64decode(request.form.to_dict()['log_file'])
-    
+    log_file = base64.b64decode(request.get_json(force=True)['content'])
+    print(log_file)
     # Split the string with respect to the newline operator "\n" to separate the individual log file entries
     raw_log_file_entries = log_file.splitlines()
 
