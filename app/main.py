b3b47c5718259781cb82cda47ef292b04fa33d3b
Pako Diale <u17076146@tuks.co.za>
2020-08-16 15:18:36 +0200

    Fixed security issue

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ from flask import Flask
 from flask_pymongo import pymongo
 #from app import app
 
-DB_CONNECTION= "mongodb://pyraspace:pyraspace2020@learning-shard-00-00-jaac5.mongodb.net:27017,learning-shard-00-01-jaac5.mongodb.net:27017,learning-shard-00-02-jaac5.mongodb.net:27017/ALFA_DB?ssl=true&replicaSet=learning-shard-0&authSource=admin&retryWrites=true&w=majority"
+DB_CONNECTION="db connection string to Mongodb goes here"
 
 client = pymongo.MongoClient(DB_CONNECTION)
 db = client.get_database('ALFA_DB')
