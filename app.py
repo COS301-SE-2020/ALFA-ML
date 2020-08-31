7d4352461d1e8e37837bc276684beb04e5816ee7
Pako Diale <u17076146@tuks.co.za>
2020-08-31 16:35:33 +0200

    experimented with a choropleth map

--- app.py
+++ app.py
@@ -1,35 +1,101 @@
-import dash
+import pandas as pd
+import plotly.express as px
+
+import dash 
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import pandas as pd
+from dash.dependencies import Input, Output
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# ------------------------------------------
+# Import and clean data
+df = pd.read_csv("intro_bees.csv")
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
+df.reset_index(inplace=True)
+print(df[:5])
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+# ------------------------------------------
+# App layout
+# remember: what does inside your 'App Layout' is your dash components i.e dropdowns, graphs, checkboxes
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+app.layout = html.Div([
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    html.H1(
+        "Web Application Dashboards with Dash", 
+        style={'text-align': 'center'}
+    ),
+
+    dcc.Dropdown(
+        id='slct_year',
+        options=[
+            {'label': '2015', 'value': 2015},
+            {'label': '2016', 'value': 2016},
+            {'label': '2017', 'value': 2017},
+            {'label': '2018', 'value': 2018}
+        ],
+        multi=False,
+        value=2015,
+        style={'width': '40%'}
+    ),
+
+    html.Div(
+        id='output_container',
+        children=[]
+    ),
+
+    html.Br(),
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='my_bee_map',
+        figure={}
     )
 ])
 
+# -------------------------------------
+# Connect the Plotly graphs with the Dash components
+@app.callback(
+    [Output(component_id='output_container', component_property='children'),
+     Output(component_id='my_bee_map', component_property='figure')],
+    [Input(component_id='slct_year', component_property='value')]   
+
+)
+# this is the function definition of the callback
+# each parameter is connected to the number of inputs -> parameter always refers to the component_property
+def update_graph(option_slctd):
+    print(option_slctd)
+    print(type(option_slctd))
+
+    # return value 1
+    container = "The year chosen by the user was: {}".format(option_slctd)
+
+    dff = df.copy()
+    dff = dff[dff['Year'] == option_slctd]
+    dff = dff[dff['Affected by'] == 'Varroa_mites']
+    
+    # Plotly Express
+    # return value 2
+    fig = px.choropleth(
+        data_frame=dff,
+        locationmode='USA-states',
+        locations='state_code',
+        scope='usa',
+        color='Pct of Colonies Impacted',
+        hover_data=['State', 'Pct of Colonies Impacted'],
+        color_continuous_scale=px.colors.sequential.YlOrRd,
+        labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
+        template='plotly_dark'
+    )
+    
+
+
+
+    # return in the order of the outputs
+    return container, fig
+
+
+
+# -------------------------------------
 if __name__ == '__main__':
     app.run_server(debug=True)
