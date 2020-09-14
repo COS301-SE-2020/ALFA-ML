00a81e79187e406f48fb0f4af1a9406609551a71
Pako Diale <u17076146@tuks.co.za>
2020-09-14 12:08:34 +0200

    Added a pie chart

--- app.py
+++ app.py
@@ -3,74 +3,84 @@ import dash.dependencies as dd
 import dash_core_components as dcc
 import dash_html_components as html
 import dash 
-import dash_table as dt
 import dash_bootstrap_components as dbc
 import plotly.express as px
 from io import BytesIO
 import pandas as pd
 from wordcloud import WordCloud
 import base64
+import plotly.graph_objects as go
 
-# define the app 
-external_stylesheets = [dbc.themes.BOOTSTRAP]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-# Constants
+# Global Constants
 PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 
-# ==============================================
-# Import the dataset or define the dataset to be used programmtically
+# define the app 
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# =========================================================================
+# Import the dataset
 # Data preparation will also take place here.
 data = {
     'error_types': ['Fatal_Error', 'Notice_Error', 'Password_Auth_Failure', 'Warning', 'Parse_Error', 'Undefined'], 
-    'frequency': [4, 8, 17, 14, 30, 21]
+    'frequency': [4, 8, 1, 14, 150, 3]
+}
+
+data1 = {
+    'error_types': ['Fatal_Error', 'Notice_Error', 'Password_Auth_Failure', 'Warning', 'Parse_Error', 'Undefined'], 
+    'frequency': [4, 8, 17, 14, 30, 21],
+    'date': ['Sat Feb 19 01:02:23.406157 2018','Sun Mar 08 01:02:23.406157 2018','Thur Jun 07 01:02:23.406157 2018','Tue Jul 12 01:02:23.406157 2018', 'Fri Aug 04 01:02:23.406157 2018','Sat Sep 11 01:02:23.406157 2018'],
+    'client': ['127.0.0.1:57668','127.0.0.1:57668','127.0.0.1:57668','127.0.0.1:57668','127.0.0.1:57668','127.0.0.1:57668']
 }
 
 dfm = pd.DataFrame(data)
+dfm1 = pd.DataFrame(data1)
 
-# ==============================================
-# Graphs, diagrams and other illustrations will be defined here
-WORD_CLOUD = html.Img(
-    id="image_wc"
-)
-
-TABLE = dbc.Table.from_dataframe(dfm, striped=True, bordered=True, hover=True)
-
-DASH_TABLE = dt.DataTable(
-    columns=[{"name": i, "id": i} for i in dfm.columns],
-    data=dfm.to_dict('records'),
-    style_cell={'textAlign': 'left'},
-    
-)
-
-import plotly.graph_objects as go
-
-colors = ['lightslategray',] * 6
-colors[1] = 'crimson'
-
-fig = go.Figure(data=[go.Bar(
-    x=data['error_types'],
-    y=data['frequency'],
-    marker_color=colors # marker color can be a single color value or an iterable
-)])
-fig.update_layout(title_text='Errors and Their Frequencies')
-
-# ==============================================
-# Metadata and attributes
+# =============================================================
+# App attributes
 colours = {
     'background': '#d3d3d3',
     'text': '#7FDBFF'
 }
 
-# ==============================================
-# Create visualisation layout and components
+colors = ['lightslategray',] * 6
+colors[1] = 'crimson'
+
+# ===========================================================================
+# Graphs, diagrams and other illustrations will be defined here
+WORD_CLOUD = html.Img(
+    id="image_wc"
+)
+
+TABLE = dbc.Table.from_dataframe(dfm1, striped=True, bordered=True, hover=True)
+
+fig_bar_chart = go.Figure(data=[go.Bar(
+    y=data['error_types'],
+    x=data['frequency'],
+    marker_color=colors, # marker color can be a single color value or an iterable
+    orientation="h"
+)])
+
+fig_bar_chart.update_layout(title_text='Errors and Their Frequencies Bar Chart')
+
+
+fig_pie_chart = go.Figure(data=[go.Pie(
+	labels=data['error_types'], 
+	values=data['frequency'], 
+	hole=.3)
+])
+
+fig_pie_chart.update_layout(title_text='Errors and Their Frequencies Pie Chart')
+
+
+# ==============================================================
+# Custom components to be appended to app layout
 NAVBAR = dbc.Navbar(
     children=[
         html.A(
             # Use row and col to control vertical alignment of logo / brand
             dbc.Row(
                 [
-                    dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
+                    dbc.Col(html.Img(src=PLOTLY_LOGO, height="40px")),
                     dbc.Col(
                         dbc.NavbarBrand("ALFA Data Science Dashboard", className="ml-2")
                     ),
@@ -86,7 +96,7 @@ NAVBAR = dbc.Navbar(
 )
 
 card_content = [
-    dbc.CardHeader("World Cloud"),
+    dbc.CardHeader("Word Cloud"),
     dbc.CardBody(
         [
             html.H5("Most Frequent Errors", className="card-title"),
@@ -96,6 +106,9 @@ card_content = [
         ]
     ),
 ]
+
+# =============================================================
+# app.layout describes what the app will look like
 app.layout = html.Div(children=[
     NAVBAR,
 
@@ -109,7 +122,15 @@ app.layout = html.Div(children=[
     html.Div(
         children=[
             dcc.Graph(
-                figure=fig,
+                figure=fig_bar_chart,
+            ),
+        ],
+    ),
+
+    html.Div(
+        children=[
+            dcc.Graph(
+                figure=fig_pie_chart,
             ),
         ],
     ),
@@ -121,15 +142,17 @@ app.layout = html.Div(children=[
         ]
     ),
 ])
+# =================================================================
 
+# create the WordCloud
 def plot_wordcloud(data):
     d = {a: x for a, x in data.values}
-    #print(d)
     wc = WordCloud(background_color=colours['background'], width=480, height=360)
     wc.fit_words(d)
     return wc.to_image()
 
-@app.callback(dd.Output('image_wc', 'src'), [dd.Input('image_wc', 'id')])
+@app.callback(dd.Output('image_wc', 'src'), 
+               [dd.Input('image_wc', 'id')])
 def make_image(b):
     img = BytesIO()
     plot_wordcloud(data=dfm).save(img, format='PNG')
