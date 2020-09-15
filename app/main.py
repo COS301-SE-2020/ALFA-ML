8ae322e2858ac47a5492ba9c4a45523e5dc4f5b6
Orifha Mbedzi <Orifha@Mbedzi.co.za>
2020-09-15 13:40:52 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ from flask import Flask
 from flask_pymongo import pymongo
 from datetime import date, datetime
 #from flask_cors import CORS, cross_origin
-from . import analyzer
+import analyzer
 DB_CONNECTION="mongodb://pyraspace:pyraspace2020@learning-shard-00-00-jaac5.mongodb.net:27017,learning-shard-00-01-jaac5.mongodb.net:27017,learning-shard-00-02-jaac5.mongodb.net:27017/ALFA_DB?ssl=true&replicaSet=learning-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 client = pymongo.MongoClient(DB_CONNECTION)
